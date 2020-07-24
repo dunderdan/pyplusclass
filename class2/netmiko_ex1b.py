700af49bb03ab456678b18d9919cbe6f2e526a68
Daniel Grubbs <pydunderdan@gmail.com>
2020-07-24 06:27:19 -0700

    Added class2 directory and initial exercise files

---
+++ class2/netmiko_ex1b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+1. Use the extended 'ping' command and Netmiko on the 'cisco4' router. This should prompt you for additional information as follows:
+
+cisco4#ping
+Protocol [ip]: 
+Target IP address: 8.8.8.8
+Repeat count [5]: 
+Datagram size [100]: 
+Timeout in seconds [2]: 
+Extended commands [n]: 
+Sweep range of sizes [n]: 
+Type escape sequence to abort.
+Sending 5, 100-byte ICMP Echos to 8.8.8.8, timeout is 2 seconds:
+!!!!!
+Success rate is 100 percent (5/5), round-trip min/avg/max = 1/1/4 ms
+
+b. Use send_command() and the expect_string argument to handle the additional prompting. Once again specify a target IP address of '8.8.8.8'.
+"""
+
---
+++ class2/netmiko_ex2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""
+2. Create a Netmiko connection to the 'nxos2' device using a global_delay_factor of 2. Execute 'show lldp neighbors detail' and print the returned output to standard output. Execute 'show lldp neighbors detail' a second time using send_command() with a delay_factor of 8. Print the output of this command to standard output. Use the Python datetime library to record the execution time of both of these commands. Print these execution times to standard output.
+"""
+
