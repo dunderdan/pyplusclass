71f293bb83c96b1009005588c6879c305b56d74f
Daniel Grubbs <pydunderdan@gmail.com>
2020-07-24 12:20:36 -0700

    Commiting completed Exercise 1a

--- class2/netmiko_ex1a.py
+++ class2/netmiko_ex1a.py
@@ -17,4 +17,30 @@ Success rate is 100 percent (5/5), round-trip min/avg/max = 1/1/4 ms
 
 a. Use send_command_timing() to handle the additional prompting from this 'ping' command. Specify a target IP address of '8.8.8.8'
 """
+from netmiko import ConnectHandler
+from getpass import getpass
+
+ip_address_to_ping = '8.8.8.8'
+
+device = {
+    'host': 'cisco4.lasthop.io',
+    'username': 'pyclass',
+    'password': getpass(),
+    'device_type': 'cisco_ios',
+    'fast_cli': True,
+}
+
+net_connect = ConnectHandler(**device)
+# print(net_connect.find_prompt())
+output = net_connect.send_command_timing('ping\n')
+output += net_connect.send_command_timing('\n')
+output += net_connect.send_command_timing(ip_address_to_ping)
+output += net_connect.send_command_timing('\n')
+output += net_connect.send_command_timing('\n')
+output += net_connect.send_command_timing('\n')
+output += net_connect.send_command_timing('\n')
+output += net_connect.send_command_timing('\n')
+
+print(output)
+net_connect.disconnect()
 
