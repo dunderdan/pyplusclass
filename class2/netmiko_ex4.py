5f7af04c05dc94aa0969540a85e9afc510c4bac4
Daniel Grubbs <pydunderdan@gmail.com>
2020-07-25 17:26:41 -0700

    Commit of completed Exercise 4

--- class2/netmiko_ex4.py
+++ class2/netmiko_ex4.py
@@ -6,8 +6,31 @@ ip name-server 1.1.1.1
 ip name-server 1.0.0.1
 ip domain-lookup
 
-Experiment with fast_cli=True to see how long the script takes to execute (with and without this option enabled).
+Experiment with fast_cli=True to see how long the script takes to execute 
+(with and without this option enabled).
 
-Verify DNS lookups on the router are now working by executing 'ping google.com'. Verify from this that you receive a ping response back.
+Verify DNS lookups on the router are now working by executing 'ping google.com'. 
+Verify from this that you receive a ping response back.
 """
+from getpass import getpass
+from netmiko import ConnectHandler
 
+
+device = {
+    'host': 'cisco3.lasthop.io',
+    'username': 'pyclass',
+    'password': getpass(),
+    'device_type': 'cisco_ios',
+    'fast_cli': True,
+}
+
+commands = ['ip name-server 1.1.1.1', 'ip name-server 1.0.0.1', 'ip domain-lookup']
+
+net_connect = ConnectHandler(**device)
+# print(net_connect.find_prompt())
+output = net_connect.send_config_set(commands)
+print(output)
+print('-' * 60)
+verify_dns = net_connect.send_command('ping google.com')
+print(verify_dns)
+print('-' * 60)
